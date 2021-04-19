db89f6ea6d7931b4d21e15849a9f3b80ab60c180
MoisesMed <br.moises@hotmail.com>
2021-04-18 23:09:14 -0300

    opcoes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
+import Radio from '@material-ui/core/Radio';
 
 import { useCount } from '../context/Count';
+
+import './Menu.css'
 
 const useStyles = makeStyles((theme) => ({
     menu: {
@@ -18,13 +21,12 @@ const useStyles = makeStyles((theme) => ({
         border: '1px solid #686868',
         borderRadius: '8px',
         margin: '0px 100px',
-
     },
     optionsTitle: {
         padding: 0,
         width: '375px',
-        height: '55px',
-
+        height: '800px',
+        overflow: 'auto'
     },
     optionsType: {
         width: '340px',
@@ -45,63 +47,86 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Menu() {
+    const [selectedValue, setSelectedValue] = React.useState('a');
+
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
     const { count, setCount } = useCount();
     const classes = useStyles();
 
     return (
         <Container className={classes.menu} style={{ marginBottom: 0 }}>
-            <Container >
-                <div className={classes.optionsTitle}>
-                    < div style={{ backgroundColor: 'rgba(253, 215, 14, 0.2)', padding: 12, marginTop: 10 }}>
-                        <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>
-                            Adicionar Ingredientes
+            <div className={classes.optionsTitle}>
+                < div style={{ backgroundColor: 'rgba(253, 215, 14, 0.2)', padding: 12, marginTop: 10 }}>
+                    <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>
+                        Adicionar Ingredientes
                         </div>
-                        <div style={{ fontSize: '12px', fontWeight: 400, color: '#E49700' }}>
-                            Até 8 ingredientes
-                        </div>
-                    </div>
-
-                    <div className={classes.optionsType}>
-                        <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>Queijo Cheddar</div>
-                        <div className={classes.buttons}>+ 2 -</div>
-                        <div style={{ fontSize: '14px', fontWeight: 400, color: '#F09035' }}>+ R$4,99</div>
-                    </div>
-
-                    <div className={classes.optionsType}>
-                        <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>Cebola crispy</div>
-                        <div className={classes.buttons}>+ 2 -</div>
-                        <div style={{ fontSize: '14px', fontWeight: 400, color: '#F09035' }}>+ R$3,50</div>
-                    </div>
-
-                    <div className={classes.optionsType}>
-                        <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>Molho cheddas</div>
-                        <div className={classes.buttons}>+ 2 -</div>
-                        <div style={{ fontSize: '14px', fontWeight: 400, color: '#F09035' }}>+ R$4,99</div>
-                    </div>
-
-                    <div className={classes.optionsType}>
-                        <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>Molho de picanha</div>
-                        <div className={classes.buttons}>+ 2 -</div>
-                        <div style={{ fontSize: '14px', fontWeight: 400, color: '#F09035' }}>+ R3,50</div>
-                    </div>
-
-                    < div style={{ backgroundColor: 'rgba(253, 215, 14, 0.2)', padding: 12, marginTop: 10 }}>
-                        <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>
-                            Precisa de talher?
-                            </div>
-                    </div>
-                    <Container style={{ display: 'flex', marginTop: 12, height: '40px', padding: 0 }} >
-                        <div style={{ width: '140px', border: '1px solid #F09035', borderRadius: '4px', padding: 7 }}>
-                            ola
-                            </div>
-
-                        <div style={{ flex: 1 }}>
-                            <Button onClick={() => setCount(count + 1)} style={{ width: '230px', height: '40px', backgroundColor: '#F09035', color: 'white', marginLeft: 16, fontSize: 14 }}>Adicionar</Button>
-                        </div>
-                    </Container>
+                    <div style={{ fontSize: '12px', fontWeight: 400, color: '#E49700' }}>
+                        Até 8 ingredientes
+                     </div>
                 </div>
-            </Container>
-        </Container>
+                <div className={classes.optionsType}>
+                    <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>Queijo Cheddar</div>
+                    <div className={classes.buttons}>+ 2 -</div>
+                    <div style={{ fontSize: '14px', fontWeight: 400, color: '#F09035' }}>+ R$4,99</div>
+                </div>
+
+                <div className={classes.optionsType}>
+                    <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>Cebola crispy</div>
+                    <div className={classes.buttons}>+ 2 -</div>
+                    <div style={{ fontSize: '14px', fontWeight: 400, color: '#F09035' }}>+ R$3,50</div>
+                </div>
+
+                <div className={classes.optionsType}>
+                    <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>Molho cheddas</div>
+                    <div className={classes.buttons}>+ 2 -</div>
+                    <div style={{ fontSize: '14px', fontWeight: 400, color: '#F09035' }}>+ R$4,99</div>
+                </div>
+
+                <div className={classes.optionsType}>
+                    <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>Molho de picanha</div>
+                    <div className={classes.buttons}>+ 2 -</div>
+                    <div style={{ fontSize: '14px', fontWeight: 400, color: '#F09035' }}>+ R3,50</div>
+                </div>
+
+                < div style={{ backgroundColor: 'rgba(253, 215, 14, 0.2)', padding: 12, marginTop: 10 }}>
+                    <div style={{ fontSize: '14px', fontWeight: 500, color: '#4E4E4E' }}>
+                        Precisa de talher?
+                            </div>
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center', padding: '0px 12px' }}>
+                    <div style={{ flex: 1 }}>Sim</div>
+                    <Radio
+                        checked={selectedValue === 'a'}
+                        onChange={handleChange}
+                        value="a"
+                        name="radio-button-demo"
+                        inputProps={{ 'aria-label': 'B' }}
+                    />
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center', padding: '0px 12px' }}>
+                    <div style={{ flex: 1 }}>Não</div>
+                    <Radio
+                        checked={selectedValue === 'b'}
+                        onChange={handleChange}
+                        value="b"
+                        name="radio-button-demo"
+                        inputProps={{ 'aria-label': 'B' }}
+                    />
+                </div>
+
+            </div>
+            <div style={{ display: 'flex', marginTop: 12, height: '40px', padding: 0 }} >
+                <div style={{ width: '140px', border: '1px solid #F09035', borderRadius: '4px', padding: 7 }}>
+                    - 2 +
+                     </div>
+                <div style={{ flex: 1 }}>
+                    <Button onClick={() => setCount(count + 1)} style={{ width: '230px', height: '40px', backgroundColor: '#F09035', color: 'white', marginLeft: 16, fontSize: 14 }}>Adicionar</Button>
+                </div>
+            </div>
+        </Container >
 
     );
 
